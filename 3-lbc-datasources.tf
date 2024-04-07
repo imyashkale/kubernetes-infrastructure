7c3f81e7fdda5cc84ccb4b176358fa501a52aba6
Yash Kale <yashkale46@gmail.com>
2024-04-07 17:09:07 +0530

    Testing Setup

--- 3-lbc-datasources.tf
+++ 3-lbc-datasources.tf
@@ -6,8 +6,3 @@ data "http" "lbc_iam_policy" {
     Accept = "application/json"
   }
 }
-
-output "lbc_iam_policy" {
-  #value = data.http.lbc_iam_policy.body
-  value = data.http.lbc_iam_policy.response_body
-}
---
+++ 9-argocd-application.tf
@@ -0,0 +1,35 @@
+resource "kubernetes_manifest" "ultra" {
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "${local.name}"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+
+      source = {
+        repoURL        = "https://github.com/imyashkale/terraform-k8s-services.git"
+        targetRevision = "HEAD"
+        path           = "dev"
+        directory = {
+          recurse : true
+        }
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "myapp"
+      }
+      syncPolicy = {
+        syncOptions = ["CreateNamespace=true"]
+
+        automated = {
+          selfHeal = true
+          prune    = true
+        }
+      }
+    }
+  }
+}
