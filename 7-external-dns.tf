e663bbda9bcbb1c0b929bdbb5bd23945439966e7
Yash Kale <yashkale46@gmail.com>
2024-04-07 15:22:10 +0530

    External DNS Record Added to Argocd Ingress

--- 7-external-dns.tf
+++ 7-external-dns.tf
@@ -63,7 +63,6 @@ resource "aws_iam_role" "externaldns_iam_role" {
   }
 }
 
-# Associate External DNS IAM Policy to IAM Role
 resource "aws_iam_role_policy_attachment" "externaldns_iam_role_policy_attach" {
   policy_arn = aws_iam_policy.externaldns_iam_policy.arn
   role       = aws_iam_role.externaldns_iam_role.name
