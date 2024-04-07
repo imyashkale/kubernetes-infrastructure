22a952ad2be3acfc5439411e37f6dd27fb2dd486
Yash Kale <yashkale46@gmail.com>
2024-04-07 16:35:57 +0530

    Helm metadata outputs removed

--- 7-external-dns.tf
+++ 7-external-dns.tf
@@ -117,8 +117,3 @@ resource "helm_release" "external_dns" {
   }
 
 }
-
-output "externaldns_helm_metadata" {
-  description = "Metadata Block outlining status of the deployed release."
-  value       = helm_release.external_dns.metadata
-}
--- outputs.tf
+++
@@ -1,5 +0,0 @@
-
-output "lbc_helm_metadata" {
-  description = "Metadata Block outlining status of the deployed release."
-  value       = helm_release.loadbalancer_controller.metadata
-}
